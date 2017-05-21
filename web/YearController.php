d5991238be2f8011159edd9f5572e56473cf4690
Adrian McDonald Tariang <adrian47mcdonald@gmail.com>
2017-05-21 23:50:42 +0530

    Added in the HttpKernel and it works like magic, will really need to look into Reflection.

--- web/YearController.php
+++ web/YearController.php
@@ -3,7 +3,7 @@
 use Symfony\Component\HttpFoundation;
 
 class YearController{
-    function leapAction($request){
+    function leapAction(HttpFoundation\Request $request){
         $year = $request->attributes->get('year');
         $answer = is_leap_year($year)? 'Leap Year': 'False';
         return new HttpFoundation\Response($answer);
--- web/front.php
+++ web/front.php
@@ -3,29 +3,34 @@
 
     use Symfony\Component\HttpFoundation;
     use Symfony\Component\Routing;
-    require 'YearController.php';
+    use Symfony\Component\HttpKernel;
+    require_once 'YearController.php';
 
     $request = HttpFoundation\Request::createFromGlobals();
+    $controllerResolver = new HttpKernel\Controller\ControllerResolver();
+    $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
 
     $routes = new Routing\RouteCollection();
     $content = new Routing\RequestContext($request);
     $routes->add('leap_year', new Routing\Route(
         '/is_leap_year/{year}',
             array('year' => 2016,
-                  '_controller' => array(new YearController(), 'leapAction'))));
+                  '_controller' => 'YearController::leapAction',)));
 
     $matcher = new Routing\Matcher\UrlMatcher($routes, $content);
     $response = new HttpFoundation\Response();
+
     try {
         $request->attributes->add(
                 $matcher->match($request->getPathInfo()));
-        $response = call_user_func($request->attributes->get('_controller'),
-                    $request);
+        $controller = $controllerResolver->getController($request);
+        $arguments = $argumentResolver->getArguments($request, $controller);
+        $response = call_user_func_array($controller, $arguments);
 
     } catch (Routing\Exception\ResourceNotFoundException $e) {
         $response->setContent("Resource not Found!");
     } catch (Exception $e) {
-        $response->setContent("Another Exception!");
+         $response->setContent("Another Exception!");
     }
 
     $response->send();
