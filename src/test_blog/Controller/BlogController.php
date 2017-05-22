5331d1e22f25d3c89e0c5cc4f25c95955e4f2fa4
Adrian McDonald Tariang <adrian47mcdonald@gmail.com>
2017-05-23 00:12:17 +0530

    Added Controller and Modal for exercise 3. Buggy.
    Need to convert to render array.

--- src/test_blog/Controller/BlogController.php
+++ src/test_blog/Controller/BlogController.php
@@ -1,24 +1,39 @@
 <?php
 namespace TestBlog\Controller;
 
-use Symfony\Component\HttpFoundation;
-use LeapYear\Modal\BlogModal;
 
+use Symfony\Component\HttpFoundation;
+use TestBlog\Modal\BlogModal;
+use PDO;
+
+//Need to change respose value.
+//Accomadate for Response Objects and render arrays.
+//tags aren't properly returned.
+//Need format for page render array.
 class BlogController{
     function blogpostAction(HttpFoundation\Request $request){
         $id = $request->attributes->get('id');
-        $answer = 'Asking for a blog page with id -> '.$id;
-        return new HttpFoundation\Response($answer);
+        $post = BlogModal::get_post_by_id($id);
+        $tag_ids = BlogModal::get_tags_by_blog_id($id);
+        $tags = BlogModal::tags_from_tag_ids($tag_ids);
+        $render_array = BlogModal::page_to_render_array($post, $tags);
+        return new HttpFoundation\Response(json_encode($tags));
     }
 
     function bloglistAction(HttpFoundation\Request $request){
-        $answer = 'Asking for a blog table listing';
-        return new HttpFoundation\Response($answer);
+        $post = BlogModal::get_blog_entries();
+        $render_array = BlogModal::table_to_render_array($port);
+        return new HttpFoundation\Response(json_encode($render_array));
     }
 
+    //need to check for wrong id.
+    //Serve the render array
     function tagstableAction(HttpFoundation\Request $request){
         $id = $request->attributes->get('id');
-        $answer = 'Asking for a blog entries with tag id -> '.$id;
-        return new HttpFoundation\Response($answer);
+        $tag = BlogModal::get_tag_by_id($id);
+        $blog_ids = BlogModal::get_blogs_by_tag_id($id);
+        $posts = BlogModal::posts_from_blog_ids($blog_ids);
+        //$render_array = BlogModal::page_to_render_array($tag, $posts);
+        return new HttpFoundation\Response(json_encode($posts));
     }
 }
--- src/test_blog/Modal/BlogModal.php
+++ src/test_blog/Modal/BlogModal.php
@@ -2,8 +2,162 @@
 
 namespace TestBlog\Modal;
 
+use Symfony\Component\HttpFoundation;
+//Haven't checked for valid Database response.
+
+use PDO;
 class BlogModal{
-    function is_leap_year($year){
-        return $year%4 == 0;
+    function start_db_connection(){
+        $conn = new PDO("mysql:host=127.0.0.1;dbname=testblog_db",
+                   'root', 'qwerty123');
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    }
+
+    function close_db_connection(&$conn) {
+        $conn = null;
+    }
+
+    function get_blog_entries() {
+        try{
+            $conn = BlogModal::start_db_connection();
+            $query = 'SELECT title, author_name FROM blog_entry';
+            $result = $conn->query($query);
+
+            $posts = $result->fetchAll(PDO::FETCH_ASSOC);
+            BlogModal::close_db_connection($conn);
+
+            return $posts;
+        }
+        catch(PDOException $e) {
+            return new HttpFoundation\Response('Resource not Found', 404);
+        }
+    }
+
+    function get_tag_entries() {
+        try{
+            $conn = BlogModal::start_db_connection();
+            $query = 'SELECT id, tag FROM tag_entries';
+            $result = $conn->query($query);
+
+            $tags = $result->fetchAll(PDO::FETCH_ASSOC);
+            BlogModal::close_db_connection($conn);
+
+            return $tags;
+        }
+        catch(PDOException $e) {
+            return new HttpFoundation\Response('Resource not Found', 404);
+        }
+    }
+
+    function table_to_render_array($posts) {
+        $render_array = array(
+                        'type' => 'table',
+                        'headers' => array_keys($posts[0]),
+                        'rows' => array(),
+                    );
+        //Setting up rows data;
+        foreach($posts as $post){
+            $render_array['rows'][] = array_values($post);
+        }
+        return $render_array;
+    }
+
+    function page_to_render_array() {
+        ;
+    }
+
+    function get_post_by_id($id){
+        try{
+            $conn = BlogModal::start_db_connection();
+            $query = 'SELECT id, title, author_name, body ';
+            $query .= 'FROM blog_entry WHERE id=:id';
+            $statement = $conn->prepare($query);
+            $statement->bindValue(':id', $id, PDO::PARAM_INT);
+            $statement->execute();
+
+            $post = $statement->fetch(PDO::FETCH_ASSOC);
+            BlogModal::close_db_connection($conn);
+            return $post;
+        }
+        catch(PDOException $e) {
+            return new HttpFoundation\Response('Resource not Found', 404);
+        }
+    }
+
+    function get_tag_by_id($id){
+        try{
+            $conn = BlogModal::start_db_connection();
+            $query = 'SELECT id, tag ';
+            $query .= 'FROM tag_entries WHERE id=:id';
+            $statement = $conn->prepare($query);
+            $statement->bindValue(':id', $id, PDO::PARAM_INT);
+            $statement->execute();
+
+            $tag = $statement->fetch(PDO::FETCH_ASSOC);
+            BlogModal::close_db_connection($conn);
+            return $tag;
+        }
+        catch(PDOException $e) {
+            return new HttpFoundation\Response('Resource not Found', 404);
+        }
+    }
+
+    function get_tags_by_blog_id($id){
+        try{
+            $conn = BlogModal::start_db_connection();
+            $query = 'SELECT id, blog_id, tag_id ';
+            $query .= 'FROM blog_tag_table WHERE blog_id=:id';
+            $statement = $conn->prepare($query);
+            $statement->bindValue(':id', $id, PDO::PARAM_INT);
+            $statement->execute();
+
+            $tag_ids = $statement->fetchAll(PDO::FETCH_ASSOC);
+            BlogModal::close_db_connection($conn);
+            return $tag_ids;
+        }
+        catch(PDOException $e) {
+            return new HttpFoundation\Response('Resource not Found', 404);
+        }
+    }
+
+    function get_blogs_by_tag_id($id){
+        try{
+            $conn = BlogModal::start_db_connection();
+            $query = 'SELECT id, blog_id, tag_id ';
+            $query .= 'FROM blog_tag_table WHERE tag_id=:id';
+            $statement = $conn->prepare($query);
+            $statement->bindValue(':id', $id, PDO::PARAM_INT);
+            $statement->execute();
+
+            $blog_ids = $statement->fetchAll(PDO::FETCH_ASSOC);
+            BlogModal::close_db_connection($conn);
+            return $blog_ids;
+        }
+        catch(PDOException $e) {
+            return new HttpFoundation\Response('Resource not Found', 404);
+        }
+    }
+
+    function tags_from_tag_ids($tag_ids) {
+        $tags_array = BlogModal::get_tag_entries();
+        $tags = array();
+
+        foreach($tag_ids as $id) {
+            $tags[] = $tags_array[$id['tag_id']]['tag'];
+        }
+
+        return $tags;
+    }
+
+    function posts_from_blog_ids($blog_ids) {
+        $posts_array = BlogModal::get_blog_entries();
+        $posts = array();
+
+        foreach($blog_ids as $id) {
+            $posts[] = $posts_array[$id['blog_id']];
+        }
+
+        return $posts;
     }
 }
