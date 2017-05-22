4a556eefe7125591b064522e6c3afd2c6165ef9d
Adrian McDonald Tariang <adrian47mcdonald@gmail.com>
2017-05-22 16:21:25 +0530

    Added the three distinct routes to the function. Tested routing; working.

--- src/leap_year/Controller/YearController.php
+++ src/leap_year/Controller/YearController.php
@@ -6,8 +6,21 @@ use LeapYear\Modal\YearModal;
 
 class YearController{
     function leapAction(HttpFoundation\Request $request){
-        $year = $request->attributes->get('year');
-        $answer = YearModal::is_leap_year($year)? 'Leap Year': 'False';
+        $id = $request->attributes->get('id');
+        //$answer = YearModal::is_leap_year($year)? 'Leap Year': 'False';
+        $answer = 'Asking for a blog page with id -> '.$id;
+        return new HttpFoundation\Response($answer);
+    }
+
+    function jumpAction(HttpFoundation\Request $request){
+        $answer = 'Asking for a blog table listing';
+        return new HttpFoundation\Response($answer);
+    }
+
+    function crawlAction(HttpFoundation\Request $request){
+        $id = $request->attributes->get('id');
+        //$answer = YearModal::is_leap_year($year)? 'Leap Year': 'False';
+        $answer = 'Asking for a blog entries with tag id -> '.$id;
         return new HttpFoundation\Response($answer);
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,15 @@
 <?php
     use Symfony\Component\Routing;
-    $routes->add('leap_year', new Routing\Route(
-    '/is_leap_year/{year}',
-        array('year' => 2016,
-              '_controller' =>
-                     'LeapYear\Controller\YearController::leapAction',)));
+    $routes->add('blog_page', new Routing\Route(
+        '/blog/{id}',
+        array('_controller' =>
+                'LeapYear\Controller\YearController::leapAction',)));
+    $routes->add('blog_listing', new Routing\Route(
+        '/blog',
+        array('_controller' =>
+                'LeapYear\Controller\YearController::jumpAction',)));
+    $routes->add('tag_id', new Routing\Route(
+        '/tag/{id}',
+        array('id' => 1,
+               '_controller' =>
+                'LeapYear\Controller\YearController::crawlAction',)));
